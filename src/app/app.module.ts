a2889ecf43d761f13c6b97b9c1a0c1e60f6df84c
Cristina Zhang <rami0141@umn.edu>
2018-06-15 14:58:21 -0500

    create-new-blog component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CreateNewBlogComponent } from './create-new-blog/create-new-blog.component';
+import { TopicsComponent } from './topics/topics.component';
+import { HomeComponent } from './home/home.component';
+import { NewBlogDisplayComponent } from './create-new-blog/new-blog-display/new-blog-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CreateNewBlogComponent,
+    TopicsComponent,
+    HomeComponent,
+    NewBlogDisplayComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/create-new-blog/create-new-blog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-new-blog',
+  templateUrl: './create-new-blog.component.html',
+  styleUrls: ['./create-new-blog.component.css']
+})
+export class CreateNewBlogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
