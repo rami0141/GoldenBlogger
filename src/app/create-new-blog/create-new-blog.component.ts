8dd09ac6873aa0e92237259b4dd231bd0108cc1c
Cristina Zhang <rami0141@umn.edu>
2018-06-16 16:39:10 -0500

    passing and using data with event binding

--- src/app/create-new-blog/create-new-blog.component.ts
+++ src/app/create-new-blog/create-new-blog.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-new-blog.component.css']
 })
 export class CreateNewBlogComponent implements OnInit {
+  blogName = '';
+  authorName = '';
+  summary = '';
+  blog = '';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onUpdateTitleName(event: any) {
+    this.blogName = event.target.value;
+  }
+  onUpdateAuthorName(event: any) {
+    this.authorName = event.target.value;
+  }
+
+  onUpdateSummary(event: any){
+    this.summary = event.target.value;
+  }
+
+  onUpdateBlog(event: any) {
+    this.blog = event.target.value;
   }
 
 }
